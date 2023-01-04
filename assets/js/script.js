42145c30df2f4a593075eb6f84230fe463cf836c
Jhinzu <jeandetorres30140@gmail.com>
2023-01-04 19:42:01 +0100

    j'ai empécher le boutton de se jouer donc la page de ce refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 // add a click event listener on the form submit button
 let boutton =(document.querySelector("#comment-submit")) ;
-let action = () =>
+let action = (boutton) =>
 {
-    alert ("bonjours");
+    boutton.preventDefault();
+    boutton.currentTarget.style.color="red";
 }
 boutton.addEventListener("click", action) ;
+
 // don't forget to "prevent" the form from being sent
 
 // create a new comment with the user name, the current date (french format: dd/mm/yyyy)
