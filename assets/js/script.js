d0e0452696297796fd2ed018e9839d12deb1b960
Jhinzu <jeandetorres30140@gmail.com>
2023-01-05 14:02:47 +0100

    j'ai terminer !

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
-// add a click event listener on the form submit button
-let boutton =(document.querySelector("#comment-submit")) ;
+let nom =document.querySelector("#comment-username") ;
+let message = document.querySelector("#comment-content") ;
+let boutton = document.querySelector("#comment-submit") ;
+let liste = document.querySelector("#comments-list") ;
+let calendrier = new Date().toLocaleDateString() ; 
 let action = (boutton) =>
 {
     boutton.preventDefault();
-    boutton.currentTarget.style.color="red";
+
+    const lii = document.createElement("li") ;
+    const h55 = document.createElement("h5") ; /* crée nos balise dans le html  */
+    const h66 = document.createElement("h6") ;
+    const pp = document.createElement("p") ; 
+
+    liste.append(lii);
+    lii.append(h55,h66,pp)                  /* mètres l'intèrieurs nos élément dans nos élément */
+    console.log (liste.innerHTML)
+
+    h55.append(nom.value) ;
+    h66.append(calendrier) ;               /* afficher la value de notre élément boutton */
+    pp.append(message.value) ;
 }
 boutton.addEventListener("click", action) ;
-
-// don't forget to "prevent" the form from being sent
-
-// create a new comment with the user name, the current date (french format: dd/mm/yyyy)
-
-// new comments must have the same HTML structure as existing comments.
